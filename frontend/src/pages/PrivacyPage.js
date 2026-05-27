7eaa18788c39830a82e9c99374b1b6c4f3119153
kajima-coder <official.abjeljac@gmail.com>
2026-05-27 12:54:37 +0200

    Create PrivacyPage.js

---
+++ frontend/src/pages/PrivacyPage.js
@@ -0,0 +1,163 @@
+PRAVILA PRIVATNOSTI
+
+Posljednje ažuriranje: 2026.
+
+Ova Pravila privatnosti opisuju kako Solvix, obrt za IT usluge, vl. Antonio Bjeljac, Zagreb, Hrvatska (u daljnjem tekstu: „mi“, „nas“ ili „Platforma“) prikuplja, koristi i štiti osobne podatke korisnika web stranice i aplikacije Solvix.
+
+Korištenjem Platforme prihvaćate ova Pravila privatnosti.
+
+1. Voditelj obrade
+
+Solvix, obrt za IT usluge
+vl. Antonio Bjeljac
+Zagreb, Hrvatska
+
+Kontakt email:
+[support@solvix.hr](mailto:support@solvix.hr)
+
+2. Podaci koje prikupljamo
+
+a) Profesionalci / izvođači usluga
+
+Možemo prikupljati:
+
+* ime i prezime
+* naziv obrta ili tvrtke
+* email adresu
+* broj telefona
+* lokaciju / grad
+* opis usluga
+* podatke povezane s rezervacijama
+
+b) Korisnici
+
+Možemo prikupljati:
+
+* ime i prezime
+* email adresu
+* broj telefona
+* podatke o rezervacijama
+* opis problema ili zahtjeva za uslugu
+
+c) Tehnički podaci
+
+Automatski možemo prikupljati:
+
+* IP adresu
+* podatke o uređaju i pregledniku
+* logove sustava
+* tehničke podatke potrebne za sigurnost i funkcioniranje Platforme
+
+3. Svrha obrade podataka
+
+Osobne podatke koristimo za:
+
+* omogućavanje rezervacija i korištenja Platforme
+* komunikaciju između korisnika i profesionalaca
+* slanje email obavijesti i podsjetnika
+* omogućavanje recenzija i ocjena
+* sigurnost sustava i sprječavanje zlouporaba
+* poboljšanje funkcionalnosti i stabilnosti Platforme
+* ispunjavanje zakonskih obveza
+
+4. Pravna osnova obrade
+
+Podaci se obrađuju na temelju:
+
+* izvršenja ugovora i pružanja usluge
+* legitimnog interesa
+* zakonskih obveza
+* privole korisnika kada je primjenjivo
+
+5. Dijeljenje podataka
+
+Podaci se mogu dijeliti s pouzdanim pružateljima usluga potrebnim za rad Platforme, uključujući:
+
+* hosting i cloud pružatelje usluga
+* email servise
+* pružatelje infrastrukture i sigurnosti
+* Stripe za obradu plaćanja i pretplata
+
+Platforma koristi usluge trećih strana uključujući, ali ne ograničavajući se na:
+
+* Vercel
+* Railway
+* MongoDB Atlas
+* SendGrid
+* Stripe
+
+Ti pružatelji mogu obrađivati određene podatke isključivo u svrhu pružanja svojih usluga.
+
+6. Plaćanja i Stripe
+
+Online plaćanja i pretplate obrađuju se putem Stripe platforme.
+
+Mi ne pohranjujemo potpune podatke o platnim karticama korisnika. Obrada plaćanja podliježe pravilima privatnosti i sigurnosnim standardima pružatelja usluge Stripe.
+
+Više informacija:
+[https://stripe.com/privacy](https://stripe.com/privacy)
+
+7. Čuvanje podataka
+
+Podatke čuvamo:
+
+* dok je korisnički račun aktivan
+* dok je potrebno za pružanje usluge
+* koliko nalažu zakonske i računovodstvene obveze
+* do zahtjeva za brisanje kada je to primjenjivo
+
+8. Prava korisnika
+
+Korisnici imaju pravo:
+
+* pristupa svojim podacima
+* ispravka netočnih podataka
+* brisanja podataka
+* ograničenja obrade
+* prigovora na obradu
+* prijenosa podataka kada je primjenjivo
+
+Zahtjeve možete poslati na:
+
+[support@solvix.hr](mailto:support@solvix.hr)
+
+9. Sigurnost podataka
+
+Poduzimamo razumne tehničke i organizacijske mjere za zaštitu osobnih podataka od neovlaštenog pristupa, gubitka, zlouporabe ili izmjene.
+
+Iako nastojimo koristiti komercijalno prihvatljive metode zaštite podataka, nijedan sustav prijenosa ili pohrane podataka nije potpuno siguran.
+
+10. Recenzije i sadržaj korisnika
+
+Recenzije mogu ostavljati samo korisnici koji su imali dovršenu rezervaciju putem Platforme.
+
+Zadržavamo pravo ukloniti sadržaj koji je:
+
+* lažan
+* obmanjujući
+* uvredljiv
+* nezakonit
+* suprotan pravilima Platforme
+
+11. Kolačići i analitika
+
+Platforma može koristiti kolačiće i osnovne analitičke alate radi:
+
+* funkcioniranja stranice
+* sigurnosti
+* analize korištenja
+* poboljšanja korisničkog iskustva
+
+Korištenjem Platforme pristajete na uporabu nužnih kolačića potrebnih za rad sustava.
+
+12. Izmjene Pravila privatnosti
+
+Zadržavamo pravo izmjene ovih Pravila privatnosti u bilo kojem trenutku.
+
+Ažurirana verzija bit će objavljena na Platformi uz novi datum ažuriranja.
+
+13. Kontakt
+
+Za sva pitanja vezana uz privatnost i obradu podataka kontaktirajte nas na:
+
+[support@solvix.hr](mailto:support@solvix.hr)
