d8755df840cee7f383baf1ca2f9f407e04a6ac80
kajima-coder <official.abjeljac@gmail.com>
2026-05-27 12:56:05 +0200

    Create TermsPage.js

---
+++ frontend/src/pages/TermsPage.js
@@ -0,0 +1,157 @@
+UVJETI KORIŠTENJA
+
+Posljednje ažuriranje: 2026.
+
+Ovi Uvjeti korištenja uređuju korištenje aplikacije i web stranice Solvix (“Platforma”), kojom upravlja Solvix, obrt za IT usluge, vl. Antonio Bjeljac, sa sjedištem u Zagrebu, Hrvatska (“mi”, “nas”, “naše”).
+
+Korištenjem Platforme prihvaćate ove Uvjete korištenja.
+
+1. Opis usluge
+
+Platforma omogućuje:
+
+profesionalcima / izvođačima usluga izradu profila i objavu usluga
+korisnicima pretraživanje profesionalaca i podnošenje zahtjeva za rezervaciju termina
+razmjenu obavijesti o rezervacijama
+ostavljanje recenzija nakon dovršene usluge
+
+Platforma je posrednička digitalna usluga između korisnika i profesionalaca.
+
+2. Tko može koristiti Platformu
+
+Platformu mogu koristiti punoljetne fizičke i pravne osobe koje mogu sklapati pravno obvezujuće odnose.
+
+Profesionalac koji otvara profil jamči da:
+
+ima pravo obavljati ponuđene usluge
+su svi uneseni podaci točni, potpuni i ažurni
+posluje u skladu s važećim zakonima Republike Hrvatske
+3. Profesionalni računi
+
+Profesionalac je odgovoran za:
+
+točnost podataka o sebi, obrtu ili poslovnom subjektu
+zakonitost usluga koje nudi
+ažuriranje cijena, dostupnosti i opisa usluga
+sigurnost svojih pristupnih podataka
+
+Zadržavamo pravo suspendirati ili ukloniti profesionalni račun ako utvrdimo:
+
+zlouporabu sustava
+netočne ili obmanjujuće podatke
+prijevarno postupanje
+kršenje ovih Uvjeta
+nezakonito korištenje Platforme
+4. Rezervacije
+
+Korisnik može putem Platforme poslati zahtjev za rezervaciju odabrane usluge i termina.
+
+Slanjem zahtjeva za rezervaciju korisnik potvrđuje da su uneseni podaci točni.
+
+Rezervacija nije konačno potvrđena dok profesionalac ne prihvati zahtjev ili dok Platforma ne prikaže potvrđeni status, ovisno o funkcionalnosti sustava.
+
+Profesionalac je odgovoran za pravovremeno prihvaćanje, odbijanje ili izvršenje rezervacije.
+
+5. Cijene, pretplate i plaćanje
+
+Cijene usluga prikazane na Platformi određuje profesionalac, osim ako je drugačije navedeno.
+
+Platforma može koristiti model pretplate za profesionalne korisnike.
+
+Plaćanja i pretplate obrađuju se putem Stripe platforme. Korištenjem pretplate korisnik prihvaća i uvjete pružatelja usluge Stripe.
+
+Ne pohranjujemo potpune podatke o platnim karticama korisnika.
+
+Zadržavamo pravo izmjene cijena, pretplata i poslovnog modela uz prethodnu objavu na Platformi.
+
+6. Recenzije
+
+Recenziju može ostaviti samo korisnik koji je imao dovršenu rezervaciju putem Platforme.
+
+Nije dopušteno:
+
+ostavljanje lažnih ili obmanjujućih recenzija
+vrijeđanje, kleveta ili govor mržnje
+objava nezakonitog ili uvredljivog sadržaja
+manipuliranje ocjenama i reputacijom korisnika
+
+Zadržavamo pravo ukloniti recenzije koje krše ove Uvjete ili važeće propise.
+
+7. Zabranjena uporaba
+
+Nije dopušteno:
+
+koristiti Platformu za nezakonite svrhe
+unositi netočne ili tuđe podatke bez ovlaštenja
+pokušavati neovlašten pristup sustavu
+ometati rad Platforme
+automatizirano napadati sustav ili zaobilaziti sigurnosne mjere
+objavljivati sadržaj koji je uvredljiv, štetan ili nezakonit
+8. Dostupnost Platforme
+
+Nastojimo održavati Platformu dostupnom i funkcionalnom, ali ne jamčimo neprekidan rad bez grešaka, prekida ili kašnjenja.
+
+Možemo privremeno obustaviti rad Platforme radi:
+
+održavanja
+nadogradnji
+sigurnosti
+tehničkih problema
+drugih opravdanih razloga
+9. Ograničenje odgovornosti
+
+U najvećoj mjeri dopuštenoj zakonom:
+
+ne jamčimo da će svaka rezervacija biti prihvaćena ili uspješno izvršena
+ne odgovaramo za kvalitetu, zakonitost ili ishod usluge koju pruža profesionalac
+ne odgovaramo za neizravnu, slučajnu ili posljedičnu štetu nastalu korištenjem Platforme
+
+Profesionalac i korisnik odgovorni su za svoj međusobni odnos, komunikaciju i izvršenje dogovorene usluge.
+
+10. Intelektualno vlasništvo
+
+Sav sadržaj Platforme, uključujući:
+
+naziv
+dizajn
+tekstove
+logotipe
+softver
+funkcionalnosti
+
+pripada nama ili našim licencodavcima, osim sadržaja koji objavljuju korisnici.
+
+Bez našeg pisanog odobrenja nije dopušteno kopirati, distribuirati, mijenjati ili komercijalno koristiti sadržaj Platforme.
+
+11. Privatnost
+
+Obrada osobnih podataka uređena je našim Pravilima privatnosti, koja su sastavni dio ovih Uvjeta korištenja.
+
+12. Prestanak korištenja
+
+Možemo ograničiti, suspendirati ili ukinuti pristup Platformi korisniku ili profesionalcu koji:
+
+krši ove Uvjete
+krši zakon
+ugrožava sigurnost sustava
+zloupotrebljava Platformu
+
+Korisnik može prestati koristiti Platformu u bilo kojem trenutku.
+
+13. Izmjene Uvjeta
+
+Zadržavamo pravo izmijeniti ove Uvjete u bilo kojem trenutku.
+
+Ažurirana verzija bit će objavljena na Platformi uz novi datum ažuriranja.
+
+14. Mjerodavno pravo i nadležnost
+
+Na ove Uvjete primjenjuje se pravo Republike Hrvatske i pravo Europske unije kada je primjenjivo.
+
+Za sporove koji se ne mogu riješiti mirnim putem nadležan je stvarno nadležni sud u Republici Hrvatskoj, osim ako prisilni propisi određuju drugačije.
+
+15. Kontakt
+
+Za sva pitanja u vezi ovih Uvjeta kontaktirajte nas na:
+
+support@solvix.hr
