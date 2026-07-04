755dbaabb6b4fc78cae8c3f5c345073f898489cb
Antonio Bjeljac <official.abjeljac@gmail.com>
2026-07-04 14:57:29 +0200

    Remove temporary debug UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,109 +21,79 @@ import TermsPage from "@/pages/TermsPage";
 
 import { AuthProvider } from "@/context/AuthContext";
 import ProtectedRoute from "@/components/ProtectedRoute";
-import DebugErrorScreen from "@/components/DebugErrorScreen";
-import DebugBoundary from "@/components/DebugBoundary";
-
-function StartupDebugBanner() {
-  return (
-    <div
-      style={{
-        position: "fixed",
-        top: 0,
-        left: 0,
-        zIndex: 999998,
-        background: "lime",
-        color: "black",
-        fontSize: 10,
-        padding: 4,
-        maxWidth: "100%",
-        overflowWrap: "break-word",
-      }}
-    >
-      React mounted: {window.location.href}
-    </div>
-  );
-}
 
 function App() {
   return (
-    <DebugBoundary>
-      <StartupDebugBanner />
+    <AuthProvider>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/pretraga" element={<SearchResultsPage />} />
+            <Route path="/registracija" element={<RegisterPage />} />
+            <Route path="/prijava" element={<LoginPage />} />
+            <Route path="/zaboravili-lozinku" element={<ForgotPasswordPage />} />
+            <Route path="/resetiraj-lozinku/:token" element={<ResetPasswordPage />} />
+            <Route path="/ocijeni/:bookingId/:token" element={<ReviewPage />} />
+            <Route path="/politika-privatnosti" element={<PrivacyPage />} />
+            <Route path="/privacy" element={<PrivacyPage />} />
+            <Route path="/uvjeti-koristenja" element={<TermsPage />} />
+            <Route path="/terms" element={<TermsPage />} />
 
-      <AuthProvider>
-        <div className="App">
-          <DebugErrorScreen />
+            <Route
+              path="/dashboard"
+              element={
+                <ProtectedRoute>
+                  <Dashboard />
+                </ProtectedRoute>
+              }
+            />
 
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<LandingPage />} />
-              <Route path="/pretraga" element={<SearchResultsPage />} />
-              <Route path="/registracija" element={<RegisterPage />} />
-              <Route path="/prijava" element={<LoginPage />} />
-              <Route path="/zaboravili-lozinku" element={<ForgotPasswordPage />} />
-              <Route path="/resetiraj-lozinku/:token" element={<ResetPasswordPage />} />
-              <Route path="/ocijeni/:bookingId/:token" element={<ReviewPage />} />
-              <Route path="/politika-privatnosti" element={<PrivacyPage />} />
-              <Route path="/privacy" element={<PrivacyPage />} />
-              <Route path="/uvjeti-koristenja" element={<TermsPage />} />
-              <Route path="/terms" element={<TermsPage />} />
+            <Route
+              path="/usluge"
+              element={
+                <ProtectedRoute>
+                  <ServicesPage />
+                </ProtectedRoute>
+              }
+            />
 
-              <Route
-                path="/dashboard"
-                element={
-                  <ProtectedRoute>
-                    <Dashboard />
-                  </ProtectedRoute>
-                }
-              />
+            <Route
+              path="/radno-vrijeme"
+              element={
+                <ProtectedRoute>
+                  <WorkingHoursPage />
+                </ProtectedRoute>
+              }
+            />
 
-              <Route
-                path="/usluge"
-                element={
-                  <ProtectedRoute>
-                    <ServicesPage />
-                  </ProtectedRoute>
-                }
-              />
+            <Route
+              path="/rezervacije"
+              element={
+                <ProtectedRoute>
+                  <BookingsPage />
+                </ProtectedRoute>
+              }
+            />
 
-              <Route
-                path="/radno-vrijeme"
-                element={
-                  <ProtectedRoute>
-                    <WorkingHoursPage />
-                  </ProtectedRoute>
-                }
-              />
+            <Route
+              path="/postavke"
+              element={
+                <ProtectedRoute>
+                  <SettingsPage />
+                </ProtectedRoute>
+              }
+            />
 
-              <Route
-                path="/rezervacije"
-                element={
-                  <ProtectedRoute>
-                    <BookingsPage />
-                  </ProtectedRoute>
-                }
-              />
+            <Route path="/majstor/:slug" element={<PublicBookingPage />} />
+            <Route path="/delete-account" element={<DeleteAccountPage />} />
+            <Route path="*" element={<LandingPage />} />
+          </Routes>
+        </BrowserRouter>
 
-              <Route
-                path="/postavke"
-                element={
-                  <ProtectedRoute>
-                    <SettingsPage />
-                  </ProtectedRoute>
-                }
-              />
-
-              <Route path="/majstor/:slug" element={<PublicBookingPage />} />
-              <Route path="/delete-account" element={<DeleteAccountPage />} />
-
-              <Route path="*" element={<LandingPage />} />
-            </Routes>
-          </BrowserRouter>
-
-          <Toaster position="top-center" richColors />
-        </div>
-      </AuthProvider>
-    </DebugBoundary>
+        <Toaster position="top-center" richColors />
+      </div>
+    </AuthProvider>
   );
 }
 
